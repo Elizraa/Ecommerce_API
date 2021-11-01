753f03576ab9c89c44bba69c4c10f14de2bbd735
Darmawan-21 <67557226+Darmawan-21@users.noreply.github.com>
2021-11-02 00:16:19 +0700

    Update OrdersService.js

--- src/services/postgres/OrdersService.js
+++ src/services/postgres/OrdersService.js
@@ -191,10 +191,10 @@ class OrdersService {
 
   async getTopBuyers() {
     const result = await this._pool.query(
-      'select buyer, sum(price),test.uname, test.profile_image from '
-      + '(Select o.id, o.userbuyer_id buyer,u.name uname,u.profile_image ,p.price from orders o '
-      + 'inner join products p on p.id = o.product_id inner join users u on u.id = o.userbuyer_id)as test '
-      + 'group by buyer,test.uname,test.profile_image order by sum DESC',
+      'select buyer_Id, sum(price),test.uname, test.profile_image from ' +
+      '(Select o.id, o.userbuyer_id buyer_Id,u.name uname,u.profile_image ,p.price from orders o ' +
+      'inner join products p on p.id = o.product_id inner join users u on u.id = o.userbuyer_id)as test '+
+      'group by buyer_Id,test.uname,test.profile_image order by sum DESC'
     );
     return result.rows;
   }
